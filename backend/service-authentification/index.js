6e0dba0476c26803c96711bee2aa209373635b71
[200~essadikine.fst@uhp.ac.ma <essadikine.fst@uhp.ac.ma>
2025-05-02 20:05:13 +0100

    init service authentification

---
+++ backend/service-authentification/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Service d\'authentification opérationnel 🚀')
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => console.log(`Serveur en écoute sur le port ${PORT}`))
