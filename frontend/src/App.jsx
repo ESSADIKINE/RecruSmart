4595138b532528c3d8b3f38b7c52e44a77b8f216
[200~essadikine.fst@uhp.ac.ma <essadikine.fst@uhp.ac.ma>
2025-05-02 19:45:50 +0100

    intialiser le frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p className="mt-4 text-gray-600">
+        React + Vite + Tailwind v4.1 is ready 🚀
+      </p>
+    </div>
+  )
+}
+
+export default App
+
